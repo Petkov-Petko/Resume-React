a62ed970dd50c588e8ab9b02cf94ae3884e4935f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-25 19:49:28 +0300

    Add new project images and update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,49 @@
 import profilePhoto from './IMG_3688.jpg';
+import franchelli from './Franchelli.png';
+import quizApp from './quizApp.png';
+import weatherApp from './weatherApp.png';
+import imageSearch from './imageSearch.png';
+import calculator from './Calculator.png';
 
 export const assets = {
     profilePhoto,
   };
   
+export const projects = [
+    {
+      title: 'Franchelli',
+      description: 'This is a project for my portfolio. Here I will showcase my projects and skills.',
+      img: franchelli,
+      url: `https://www.google.com`,
+      github: `https://github.com/Petkov-Petko/Franchelli`
+    },
+    {
+      title: 'Quiz App',
+      description: 'Quiz App. Learn everything for your Front End interview',
+      img: quizApp,
+      url: `https://petkov-petko.github.io/quizApp2/`,
+      github: `https://github.com/Petkov-Petko/quizApp2`
+    },
+    {
+      title: 'Weather App',
+      description: 'This is a simple weather app that retrieves weather data from the OpenWeather API based on user input.',
+      img: weatherApp,
+      url: `https://petkov-petko.github.io/Weather-conditions/`,
+      github: `https://github.com/Petkov-Petko/Weather-conditions`
+    },
+    {
+      title: 'Image Search',
+      description: 'Simple photo searcher using Unsplash API.',
+      img: imageSearch,
+      url: `https://petkov-petko.github.io/Image-Search/`,
+      github: `https://github.com/Petkov-Petko/Image-Search`
+    },
+    {
+      title: 'Calculator',
+      description: 'Web Calculator',
+      img: calculator,
+      url: `https://petkov-petko.github.io/Calculator/`,
+      github: `https://github.com/Petkov-Petko/Calculator`
+    },
+   
+]
