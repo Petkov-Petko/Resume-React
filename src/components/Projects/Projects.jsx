fb0529264f3df1a8f611f81e453a127010b7d400
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-25 17:25:50 +0300

    Update Projects component styling and add Projects.jsx file

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Projects.css";
+import { projects } from "../../assets/assets";
+
+const Projects = () => {
+  const [numProjectsToShow, setNumProjectsToShow] = useState(6);
+
+  const handleSeeMore = () => {
+    setNumProjectsToShow(numProjectsToShow + 6);
+  };
+
+  return (
+    <div className="projects">
+      <h1>Projects:</h1>
+      <div className="allProjects">
+        {projects.slice(0, numProjectsToShow).map((project, index) => {
+          return (
+            <div
+              key={index}
+              className="project"
+              data-title={project.description}
+            >
+              <img src={project.img} alt="Project Image"></img>
+              <div>
+                <a href={project.url} target="_blank" rel="noreferrer">
+                  <i className="fa-solid fa-link fa-lg"></i>
+                </a>
+                <a href={project.github} target="_blank" rel="noreferrer">
+                  <i className="fa-brands fa-github fa-lg"></i>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {numProjectsToShow < projects.length && (
+        <button className="btn" onClick={handleSeeMore}>
+          Show more
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Projects;
