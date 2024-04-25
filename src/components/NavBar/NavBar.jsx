995bad36219fe6729812b785d629ec9392d33f6a
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-25 14:56:39 +0300

    Add NavBar component with CSS and JSX files

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+    <ul>
+      <li><a href="#title">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#projects">Work</a></li>
+      <li><a href="#contacts">Contact</a></li>
+    </ul>
+  </div>
+  )
+}
+
+export default NavBar
