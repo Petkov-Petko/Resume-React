3a4922eef2b5d861a9486c48be1406413768b470
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-25 19:49:44 +0300

    Add Contact component and update Home page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,22 @@ import Header from '../../components/Header/Header'
 import NavBar from '../../components/NavBar/NavBar'
 import About from '../../components/About/About'
 import Projects from '../../components/Projects/Projects'
+import Contact from '../../components/Contact/Contact'
+import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
   return (
-    <div className='homePage'>
+    <div className='homePage' id='home'>
         <NavBar />
         <Header />
         <hr/>
         <About />
         <hr></hr>
         <Projects />
+        <hr></hr>
+        <Contact />
+        <hr></hr>
+        <Footer />
     </div>
   )
 }
