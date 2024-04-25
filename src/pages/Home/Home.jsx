aead43955a077b053c0164cb02289264ba6b4c88
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-25 15:37:04 +0300

    Add About component and update Home page styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import NavBar from '../../components/NavBar/NavBar'
+import About from '../../components/About/About'
 
 const Home = () => {
   return (
     <div className='homePage'>
         <NavBar />
         <Header />
+        <hr/>
+        <About />
+        <hr></hr>
     </div>
   )
 }
