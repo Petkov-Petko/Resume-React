fb82d0a5f593aa9a5ae9d30baa7697d0429197b7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-13 16:03:33 +0300

    Update About component content and improve code structure

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './About.css'
 
 const About = () => {
@@ -6,7 +5,7 @@ const About = () => {
     <div className='about' id='about'>
         <div className='leftSide'>
             <h3>About Me:</h3>
-            <p>I'm an enthusiastic 25 year old front-end developer based in Bulgaria, passionate about JavaScript and React development.I'm currently enrolled into the Alpha JS program in Telerik Academy. I'm driven by a thirst for knowledge and a passion for problem-solving, pushing myself to master new technologies and tackle challenges head-on.</p>
+            <p>I&apos;m an enthusiastic 25 year old front-end developer based in Bulgaria, passionate about JavaScript and React development. I have graduated from the Alpha JS program in Telerik Academy. I&apos;m driven by a thirst for knowledge and a passion for problem-solving, pushing myself to master new technologies and tackle challenges head-on.</p>
             <h3>Education</h3>
             <p>Telerik Academy - Jan 2024</p>
             <p>Nikola Vaptsarov Naval Academy - Oct 2022</p>
@@ -17,16 +16,15 @@ const About = () => {
         </div>
         <div className='rightSide'>
         <h3>Technical Proficiency:</h3>
-        <p>Experienced in JavaScript, HTML, CSS, and a touch of Python. Currently delving into React development and exploring TypeScript and server-side technologies.</p>
+        <p>Proficient in JavaScript, HTML, CSS, React, and Firebase. Currently expanding my skills in TypeScript and server-side technologies.</p>
         <h3>Languages and Tools:</h3>
-        <div class="tools">
+        <div className="tools">
             <span>JavaScript</span>
             <span>React</span>
             <span>HTML5</span>
             <span>CSS3</span>
             <span>TypeScript</span>
             <span>Node.js</span>
-            <span>Express.js</span>
             <span>Git</span>
             <span>Jest</span>
             <span>Eslint</span>
