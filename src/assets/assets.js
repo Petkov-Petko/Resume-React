092d3fa62dcce53ab0106463b90b7535de558757
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-05 10:46:07 +0300

    added new project

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,11 +6,12 @@ import imageSearch from './imageSearch.png';
 import calculator from './Calculator.png';
 import reactHub from './ReactHub.png';
 import calendra from './calendra.png';
+import estato from './canvaestate.png'
 
 export const assets = {
-    profilePhoto,
-  };
-  
+  profilePhoto,
+};
+
 export const projects = [
   {
     title: 'Calendra',
@@ -26,40 +27,47 @@ export const projects = [
     url: `https://reactduo-forumproject.web.app/`,
     github: `https://github.com/React-Duo/Forum-Project`
   },
-    {
-      title: 'Franchelli',
-      description: 'React-based food website.Current version focuses on the front end, future updates will include backend functionality',
-      img: franchelli,
-      url: `https://petkov-petko.github.io/Franchelli/`,
-      github: `https://github.com/Petkov-Petko/Franchelli`
-    },
-    {
-      title: 'Quiz App',
-      description: 'Quiz App. Learn everything for your Front End interview',
-      img: quizApp,
-      url: `https://petkov-petko.github.io/quizApp2/`,
-      github: `https://github.com/Petkov-Petko/quizApp2`
-    },
-    {
-      title: 'Weather App',
-      description: 'This is a simple weather app that retrieves weather data from the OpenWeather API based on user input.',
-      img: weatherApp,
-      url: `https://petkov-petko.github.io/Weather-conditions/`,
-      github: `https://github.com/Petkov-Petko/Weather-conditions`
-    },
-    {
-      title: 'Image Search',
-      description: 'Simple photo searcher using Unsplash API.',
-      img: imageSearch,
-      url: `https://petkov-petko.github.io/Image-Search/`,
-      github: `https://github.com/Petkov-Petko/Image-Search`
-    },
-    {
-      title: 'Calculator',
-      description: 'Web Calculator',
-      img: calculator,
-      url: `https://petkov-petko.github.io/Calculator/`,
-      github: `https://github.com/Petkov-Petko/Calculator`
-    },
-   
+  {
+    title: 'Estato',
+    description: 'Real estate platform using React, TypeScript, and Firebase for renting and buying properties. Currently under development.',
+    img: estato,
+    url: `https://realestate-dcdcb.firebaseapp.com/`,
+    github: `https://github.com/Petkov-Petko/RealEstate`
+  },
+  {
+    title: 'Franchelli',
+    description: 'React-based food website.Current version focuses on the front end, future updates will include backend functionality',
+    img: franchelli,
+    url: `https://petkov-petko.github.io/Franchelli/`,
+    github: `https://github.com/Petkov-Petko/Franchelli`
+  },
+  {
+    title: 'Quiz App',
+    description: 'Quiz App. Learn everything for your Front End interview',
+    img: quizApp,
+    url: `https://petkov-petko.github.io/quizApp2/`,
+    github: `https://github.com/Petkov-Petko/quizApp2`
+  },
+  {
+    title: 'Weather App',
+    description: 'This is a simple weather app that retrieves weather data from the OpenWeather API based on user input.',
+    img: weatherApp,
+    url: `https://petkov-petko.github.io/Weather-conditions/`,
+    github: `https://github.com/Petkov-Petko/Weather-conditions`
+  },
+  {
+    title: 'Image Search',
+    description: 'Simple photo searcher using Unsplash API.',
+    img: imageSearch,
+    url: `https://petkov-petko.github.io/Image-Search/`,
+    github: `https://github.com/Petkov-Petko/Image-Search`
+  },
+  {
+    title: 'Calculator',
+    description: 'Web Calculator',
+    img: calculator,
+    url: `https://petkov-petko.github.io/Calculator/`,
+    github: `https://github.com/Petkov-Petko/Calculator`
+  },
+
 ]
